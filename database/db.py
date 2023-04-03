6eb33a83b79f1028ffb85b6238287b0e7dd72c54
Jose Tovar <jose.jalisoft@gmail.com>
2023-04-02 22:27:07 -0500

    change db

--- database/db.py
+++ database/db.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# DATABASE_URL = "postgresql://postgres:temporal123@sales-app.ceiahbbslvkq.us-east-2.rds.amazonaws.com:5432/postgres"
-DATABASE_URL = "postgresql://postgres:temporal123@localhost/inventory_app"
+DATABASE_URL = "postgresql://postgres:temporal123@sales-app.ceiahbbslvkq.us-east-2.rds.amazonaws.com:5432/postgres"
+# DATABASE_URL = "postgresql://postgres:temporal123@localhost/inventory_app"
 # DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
